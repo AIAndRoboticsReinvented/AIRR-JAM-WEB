99b693883f88c4b6822bb37255356777a6c0badb
Richard Yager <richardyager@msn.com>
2017-03-06 19:26:45 -0500

    Appended the I Buy As Is Logo

--- WebSite/WebSite/src/app/mock-team.ts
+++ WebSite/WebSite/src/app/mock-team.ts
@@ -11,16 +11,16 @@ export const TEAM: Teammember[] = [
         region: '',
         about: 'My entrepreneur spirit began early on and throughout the years I have owned different businesses including an alarm company and a Golden Corral restaurant.  Despite my background being in electronics, management, and construction, real estate remained my passion.  I began my real estate career in 2004 on a part-time basis and in 2007 I entered the real estate arena full-time.  Some of my past real estate ventures includes owning rental properties, property management, flipping a mobile home park, and renovating and building single family homes.  Currently I own and manage rental properties, wholesale and renovate single family homes as well as building new homes.  I have a wealth of information and experience which I passionately share with others.  I am personally rewarded when I spend time talking with home owners, buyers, and investors and am able to help them reach their goals.  I built my businesses around systems which allows me to work efficiently and maximize precious time.  I enjoy seeing properties and bare land transformed into a beautiful, high quality designed homes that buyers desire. It has been an exciting trip and yet the journey has just begun!'
     },
-    {
-        name: 'M.J. Sklaruk',
-        title: 'Management',
-        image: 'man.jpg',
-        passions: '',
-        moto: '',
-        quote: '',
-        region: '',
-        about: 'MJ has been investing in real estate since the year 2000 and over the past 16 years has been fortunate enough to buy and sell over 600 homes, almost exclusively in the Tampa Bay market. He has experience in remodeling and retailing over 100 properties, wholesaling over 500 properties, including a few commercial buildings but mostly residential and even building a handful of homes. The biggest attribute he considers a strength is his ability to communicate well and serve his clients’ interests as it pertains to them finding a bargain home to live in or as an investment to profit upon.MJ enjoys helping people achieve their goals and if he can assist someone with finding their new home at an affordable price, one they may not otherwise be able to buy, then that is very rewarding to him.'
-    },
+    //{
+    //    name: 'M.J. Sklaruk',
+    //    title: 'Management',
+    //    image: 'man.jpg',
+    //    passions: '',
+    //    moto: '',
+    //    quote: '',
+    //    region: '',
+    //    about: 'MJ has been investing in real estate since the year 2000 and over the past 16 years has been fortunate enough to buy and sell over 600 homes, almost exclusively in the Tampa Bay market. He has experience in remodeling and retailing over 100 properties, wholesaling over 500 properties, including a few commercial buildings but mostly residential and even building a handful of homes. The biggest attribute he considers a strength is his ability to communicate well and serve his clients’ interests as it pertains to them finding a bargain home to live in or as an investment to profit upon.MJ enjoys helping people achieve their goals and if he can assist someone with finding their new home at an affordable price, one they may not otherwise be able to buy, then that is very rewarding to him.'
+    //},
     {
         name: 'Russ Brantmyer',
         title: 'Acquisitions Director',
@@ -50,5 +50,15 @@ export const TEAM: Teammember[] = [
         quote: '',
         region: '',
         about: 'Chelsea was born in central Florida but made Tampa Bay her home in the early part of 2016. She attended college at the University of Florida where she began the Registered Nursing program. As she reached completion, she realized the field was simply not for her. Chelsea then began the process of transitioning into a MBA program of which she will complete in the next year. She has a strong background for office management and business administration and has spent most of her career managing contractors for IBM until her recent transition into the real estate industry.'
+    },   
+    {
+        name: 'Richard Yager',
+        title: 'Information Technology',
+        image: 'team/me.jpg',
+        passions: 'AI & Robotics',
+        moto: 'Be yourself, everyone else is taken.',
+        quote: 'Nothing in the world can take the place of Persistence. Talent will not; nothing is more common than unsuccessful men with talent. Genius will not; unrewarded genius is almost a proverb. Education will not; the world is full of educated derelicts. Persistence and determination alone are omnipotent. The slogan "Press On" has solved and always will solve the problems of the human race. ',
+        region: 'Call North America home, with a special fondness for; New Jersey, Pittsburgh, Texas, and Florida. Prepared to bug out in Tennessee and live out the rest of days off grid.',
+        about: 'A native born American, with long lived expertise in many aspects of computer science and artifical intellegence. Has lead the full life cycle development of systems that will be in use far after he expires. With computers, there is no favorite operating system, be it Windows, Linux or IOS the are all loved equally. A computer multlinguist including, binary, c, c++, vb, basic, sql, pl-sql, html, asp, jsp, java, javascript, xml, json, xslt, css, html5, fortran, cobal, pascal, tape basic, cpm, machine language, cold fusion, type script, AngularJS, Angular 2, Ember; the list really just goes on and on. Extreme database manipulator and general techno guru.'
     },   
 ];
